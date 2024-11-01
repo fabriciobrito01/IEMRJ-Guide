e7838d0d1a76959500ac76c7eb8d2a281bfd0da5
Fabrício de Brito <fabriciobef@hotmail.com>
2024-11-01 15:46:29 -0300

    header up

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,46 +1,100 @@
-import styled from 'styled-components';
+/*************  ✨ Codeium Command ⭐  *************/
+import { useState } from 'react';
 
-const Navbar = styled.nav`
-  display: flex;
-  align-items: center;
-  column-gap: 60px;
-  justify-content: center;
-  width: 100%;
-  background: rgb(16, 28, 102);
-  padding: 20px;
-  position: fixed;
-  top: 0;
-  z-index: 1000;
-`;
-
-const NavItem = styled.a`
-  font-size: 16px;
-  font-weight: 600;
-  letter-spacing: 2px;
-  text-transform: uppercase;
-  color: white;
-
-  &:hover {
-    color: #FFEE51;
-  }
-`;
-
-const Entrar = styled.a`
-
-`;
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <>
-      <Navbar>
-        <NavItem href='home'>Home</NavItem>
-        <NavItem href='programacao'>Programação</NavItem>
-        <NavItem href='mapa'>Mapa</NavItem>
-        <NavItem href='hospedagem'>Hospedagem</NavItem>
-        <NavItem href='times'>Times</NavItem>
-        <NavItem href='entrar'>Entrar</NavItem>
-      </Navbar>
-    </>
+    <header className="header">
+      <div className="header-icon"></div>
+      <div className="header-menu" onClick={() => setMenuOpen(!menuOpen)}>
+        <div className="header-menu-icon">
+          <span className="header-menu-icon-line"></span>
+          <span className="header-menu-icon-line"></span>
+          <span className="header-menu-icon-line"></span>
+        </div>
+        {menuOpen && (
+          <div className="header-menu-options">
+            <ul>
+              
+            </ul>
+          </div>
+        )}
+      </div>
+      <style jsx>{`
+                .header {
+                    position: fixed;
+                    top: 0;
+                    left: 0;
+                    width: 100%;
+                    background-color: rgb(16, 28, 102);
+                    padding: 10px;
+                    display: flex;
+                    justify-content: space-between;
+                    align-items: center;
+                }
+
+                .header-icon {
+                    width: 40px;
+                    height: 40px;
+                    background-color: #444;
+                    border-radius: 50%;
+                }
+
+                .header-menu {
+                    display: flex;
+                    align-items: center;
+                    cursor: pointer;
+                }
+
+                .header-menu-icon {
+                    position: relative;
+                    right: 18px;
+                    width: 40px;
+                    height: 40px;
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    align-items: center;
+                }
+
+                .header-menu-icon-line {
+                    width: 80%;
+                    height: 3px;
+                    background-color: #fff;
+                    margin: 5px 0;
+                }
+
+                .header-menu-options {
+                    position: absolute;
+                    top: 60px;
+                    left: 0;
+                    width: 100vw;
+                    height: 50vh;
+                    background-color: rgb(16, 28, 102);
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    border-top: 7px  rgb(01, 01, 40) solid;
+                }
+
+                .header-menu-options ul {
+                    list-style: none;
+                    padding: 0;
+                    margin: 0;
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                }
+
+                .header-menu-options li {
+                    margin: 10px;
+                    color: #fff;
+                }
+            `}</style>
+    </header>
   );
 }
 
 export default Header;
+/******  067b6ff2-90e5-48ae-be97-fa56c2db6d50  *******/
